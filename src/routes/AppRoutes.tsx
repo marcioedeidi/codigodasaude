5f6987be5060aea52be451f6a3665b94236b1857
marcioedeidi <marcioedeidilara@gmail.com>
2026-08-17 15:11:39 -0300

    fix: abrir páginas no topo após login e navegação

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Home from '../pages/Home'
 import AreaHome from '../pages/AreaHome'
 import Cadastro from '../pages/Cadastro'
@@ -11,9 +11,20 @@ import Sobre from '../pages/Sobre'
 import ProtectedLayout from '../components/layout/ProtectedLayout'
 import Footer from '../components/layout/Footer'
 
+const ScrollToTop: React.FC = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
+
 const AppRoutes: React.FC = () => {
   return (
     <Router basename={import.meta.env.BASE_URL}>
+      <ScrollToTop />
       <Routes>
         <Route
           path="/"
