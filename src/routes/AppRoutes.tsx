e4b41a001a95d6c796faa3467d0403dbc6aac8dd
marcioedeidi <marcioedeidilara@gmail.com>
2026-08-18 16:09:18 -0300

    Remover rota da página individual Fiber Slim

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,7 +4,6 @@ import Home from '../pages/Home'
 import AreaHome from '../pages/AreaHome'
 import Cadastro from '../pages/Cadastro'
 import Produtos from '../pages/Produtos'
-import FiberSlim from '../pages/FiberSlim'
 import Depoimentos from '../pages/Depoimentos'
 import Sobre from '../pages/Sobre'
 import ProtectedLayout from '../components/layout/ProtectedLayout'
@@ -72,7 +71,6 @@ const AppRoutes: React.FC = () => {
         <Route element={<ProtectedLayout />}>
           <Route path="/inicio" element={<AreaHome />} />
           <Route path="/produtos" element={<Produtos />} />
-          <Route path="/fiberslim" element={<FiberSlim />} />
           <Route path="/depoimentos" element={<Depoimentos />} />
           <Route path="/ebooks" element={<Navigate to="/inicio#ebooks" replace />} />
           <Route path="/sobre" element={<Sobre />} />
