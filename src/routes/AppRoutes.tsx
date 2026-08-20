f0a704fb43d72b6d1d5adcdfe7f0ebe28175bea0
marcioedeidi <marcioedeidilara@gmail.com>
2026-08-20 18:30:36 -0300

    Adicionar rota da página Velmo Caps

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,6 +6,7 @@ import Cadastro from '../pages/Cadastro'
 import Produtos from '../pages/Produtos'
 import VelmoBlackDrink from '../pages/VelmoBlackDrink'
 import ComboVelmoBlack from '../pages/ComboVelmoBlack'
+import VelmoCaps from '../pages/VelmoCaps'
 import Depoimentos from '../pages/Depoimentos'
 import Sobre from '../pages/Sobre'
 import ProtectedLayout from '../components/layout/ProtectedLayout'
@@ -75,6 +76,7 @@ const AppRoutes: React.FC = () => {
           <Route path="/produtos" element={<Produtos />} />
           <Route path="/produtos/velmo-black-drink" element={<VelmoBlackDrink />} />
           <Route path="/produtos/combo-velmo-black" element={<ComboVelmoBlack />} />
+          <Route path="/produtos/velmo-caps" element={<VelmoCaps />} />
           <Route path="/depoimentos" element={<Depoimentos />} />
           <Route path="/ebooks" element={<Navigate to="/inicio#ebooks" replace />} />
           <Route path="/sobre" element={<Sobre />} />
