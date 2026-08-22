fe5a7131418babbbc14ad0d73e1aaeb59e7cf2f5
marcioedeidi <marcioedeidilara@gmail.com>
2026-08-22 19:54:50 -0300

    Criar página do CreaGym com a nova imagem do produto

---
+++ src/pages/CreaGym.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import productImage from '../assets/Produtos/creagym.jpg'
+
+const CreaGym: React.FC = () => {
+  return (
+    <main className="w-full bg-[#f3f8f3]">
+      <section className="w-full px-4 py-8 sm:px-6 md:py-12">
+        <div className="mx-auto w-full max-w-[1400px]">
+          <div className="overflow-hidden rounded-[32px] bg-white shadow-[0_18px_55px_rgba(15,91,82,0.12)]">
+            <img src={productImage} alt="CreaGym" className="block h-auto w-full object-contain" draggable={false} />
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default CreaGym
