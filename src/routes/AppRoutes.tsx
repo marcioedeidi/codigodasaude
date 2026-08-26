d18074d0494a20f542d9454573677f51a9e08ac8
marcioedeidi <marcioedeidilara@gmail.com>
2026-08-26 14:07:24 -0300

    Conectar página Movimint à rota do site

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -12,6 +12,7 @@ import FiberSlim from '../pages/FiberSlim'
 import ThermoDrink from '../pages/ThermoDrink'
 import CreaGym from '../pages/CreaGym'
 import SkinFit from '../pages/SkinFit'
+import Movimint from '../pages/movimint'
 import Depoimentos from '../pages/Depoimentos'
 import Sobre from '../pages/Sobre'
 import ProtectedLayout from '../components/layout/ProtectedLayout'
@@ -62,6 +63,7 @@ const AppRoutes: React.FC = () => {
           <Route path="/produtos/thermo-drink" element={<ThermoDrink />} />
           <Route path="/produtos/creagym" element={<CreaGym />} />
           <Route path="/produtos/skin-fit" element={<SkinFit />} />
+          <Route path="/produtos/movimint" element={<Movimint />} />
           <Route path="/depoimentos" element={<Depoimentos />} />
           <Route path="/ebooks" element={<Navigate to="/inicio#ebooks" replace />} />
           <Route path="/sobre" element={<Sobre />} />
