44b960905f39060ab355f302d0611bc458bd40e6
marcioedeidi <marcioedeidilara@gmail.com>
2026-09-02 02:26:23 -0300

    Corrigir posicionamento dos links CreaGym primeiro e segundo botao

--- src/pages/CreaGym.tsx
+++ src/pages/CreaGym.tsx
@@ -6,7 +6,7 @@ const CreaGym: React.FC = () => {
     <main className="w-full bg-[#f3f8f3]">
       <section className="w-full px-4 py-8 sm:px-6 md:py-12">
         <div className="mx-auto w-full max-w-[1400px]">
-          <div className="relative overflow-hidden rounded-[32px] bg-white shadow-[0-18px-55px_rgba(15,91,82,0.12)]">
+          <div className="relative overflow-hidden rounded-[32px] bg-white shadow-[0_18px_55px_rgba(15,91,82,0.12)]">
             <img src={productImage} alt="CreaGym" className="block h-auto w-full object-contain" draggable={false} />
 
             <div className="absolute inset-0">
