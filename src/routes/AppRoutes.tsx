06290dfbf2257b73a96d64724e8cd25ea7e92989
marcioedeidi <marcioedeidilara@gmail.com>
2026-09-15 22:35:13 -0300

    Liberar acesso publico a pagina de produtos e Pro Curcumin

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -35,11 +35,13 @@ const AppRoutes: React.FC = () => (
     <Routes>
       <Route path="/" element={<><Home /><Footer /></>} />
       <Route path="/cadastro" element={<Cadastro />} />
+
+      <Route path="/produtos" element={<Produtos />} />
+      <Route path="/produtos/pro-curcumin" element={<ProCurcumin />} />
       <Route path="/produtos/alwaysfit" element={<AlwaysFit />} />
+
       <Route element={<ProtectedLayout />}>
         <Route path="/inicio" element={<AreaHome />} />
-        <Route path="/produtos" element={<Produtos />} />
-        <Route path="/produtos/pro-curcumin" element={<ProCurcumin />} />
         <Route path="/produtos/velmo-black-drink" element={<VelmoBlackDrink />} />
         <Route path="/produtos/combo-velmo-black" element={<ComboVelmoBlack />} />
         <Route path="/produtos/celuglow" element={<Celuglow />} />
