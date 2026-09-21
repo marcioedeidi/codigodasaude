0b5898b04d886170cdefe3d05bda5afcb786cb42
marcioedeidi <marcioedeidilara@gmail.com>
2026-09-21 18:00:57 -0300

    Adiciona pagina do Picolinato de Cromo AlwaysFit

---
+++ src/pages/PicolinatoCromo.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const PicolinatoCromo: React.FC = () => {
+  return (
+    <main className="w-full bg-[#f3f8f3]">
+      <section className="w-full">
+        <div className="mx-auto w-full">
+          <div className="relative overflow-hidden">
+            <img
+              src="/assets/alwaysfit/picolinato-cromo.svg"
+              alt="AlwaysFit Picolinato de Cromo"
+              className="block h-auto w-full object-contain"
+              draggable={false}
+            />
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default PicolinatoCromo
