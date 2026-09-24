b85f21b9bc2acbef468ddb780561701bec5cd2c2
marcioedeidi <marcioedeidilara@gmail.com>
2026-09-24 18:26:45 -0300

    Adiciona barra fixa no topo à página Pro3 Magnésio

--- src/pages/Pro3Magnesio.tsx
+++ src/pages/Pro3Magnesio.tsx
@@ -1,34 +1,39 @@
 import React from 'react'
 import productImage from '../assets/Produtos/alwaysfit-manesio-vitaminad.jpeg'
 import footerImage from '../assets/footer/rodape.png'
+import Navbar from '../components/layout/Navbar'
 
 const Pro3Magnesio: React.FC = () => {
   return (
-    <main className="w-full bg-[#f3f8f3]">
-      <section className="w-full">
-        <div className="mx-auto w-full">
-          <div className="relative overflow-hidden">
+    <>
+      <Navbar />
+
+      <main className="w-full bg-[#f3f8f3]">
+        <section className="w-full">
+          <div className="mx-auto w-full">
+            <div className="relative overflow-hidden">
+              <img
+                src={productImage}
+                alt="AlwaysFit Pro3 Magnésio + Vitamina D3 + K2"
+                className="block h-auto w-full object-contain"
+                draggable={false}
+              />
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="mx-auto w-full">
             <img
-              src={productImage}
-              alt="AlwaysFit Pro3 Magnésio + Vitamina D3 + K2"
-              className="block h-auto w-full object-contain"
+              src={footerImage}
+              alt="Código da Saúde"
+              className="block w-full"
               draggable={false}
             />
           </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="mx-auto w-full">
-          <img
-            src={footerImage}
-            alt="Código da Saúde"
-            className="block w-full"
-            draggable={false}
-          />
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   )
 }
 
