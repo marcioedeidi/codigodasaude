174c5f8f7444c5cf18d139464b1139d1efa10982
marcioedeidi <marcioedeidilara@gmail.com>
2026-09-24 18:19:23 -0300

    Corrige imagem individual da página Pro3 Magnésio

--- src/pages/Pro3Magnesio.tsx
+++ src/pages/Pro3Magnesio.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
+import productImage from '../assets/Produtos/alwaysfit-manesio-vitaminad.jpeg'
 
 const Pro3Magnesio: React.FC = () => {
-  const productImage =
-    'https://alwaysfit.com.br/cdn/shop/files/alwaysfit-pro3-1frasco_1__11zon_1c68d9d8-5d77-4f5b-85c9-69b3cd767ca1_1200x.webp?v=1783636176'
-
   return (
     <main className="w-full bg-[#f3f8f3]">
       <section className="w-full">
