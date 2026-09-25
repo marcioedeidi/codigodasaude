824572c5ac5e810fbfc0e553d341ab1794f599af
marcioedeidi <marcioedeidilara@gmail.com>
2026-09-24 22:20:36 -0300

    Cria página do produto AlwaysFit NAC

---
+++ src/pages/Nac.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import productImage from '../assets/Produtos/always-fit-nac.jpeg'
+import footerImage from '../assets/footer/rodape.png'
+import Navbar from '../components/layout/Navbar'
+
+const Nac: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+
+      <main className="w-full bg-[#f3f8f3]">
+        <section className="w-full">
+          <div className="mx-auto w-full">
+            <div className="relative overflow-hidden">
+              <img
+                src={productImage}
+                alt="AlwaysFit NAC 600mg"
+                className="block h-auto w-full object-contain"
+                draggable={false}
+              />
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="mx-auto w-full">
+            <img
+              src={footerImage}
+              alt="Código da Saúde"
+              className="block w-full"
+              draggable={false}
+            />
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default Nac
